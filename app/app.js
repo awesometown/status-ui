7af4da4fc4295c484a6efb60b1da74a7f1cd446b
John Price <joprice@ea.com>
2015-10-27 17:36:08 -0400

    Figuring stuff out

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Dashboard from "./dashboard";
+require("./theme.css");
+
+ReactDOM.render(
+	<Dashboard/>,
+	document.getElementById('dashboard')
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	context: __dirname + "/app",
+	entry: "./app.js",
+
+	output: {
+		filename: "app.js",
+		path: __dirname + "/dist",
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: ["react-hot", "babel-loader"]
+			},
+			{
+				test: /\.css$/,
+				loader: "style!css"
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: ["babel-loader"],
+			},
+			{
+				test: /\.html$/,
+				loader: "file?name=[name].[ext]",
+			},
+
+			{test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+			{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+			{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+		]
+	}
+}
