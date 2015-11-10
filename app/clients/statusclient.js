e68f24374546f2fe3838f263b79991dadeb2b78e
John Price <john.price@gmail.com>
2015-11-09 23:28:38 -0500

    Updated status client and added basic host selection logic

--- app/clients/statusclient.js
+++ app/clients/statusclient.js
@@ -9,11 +9,31 @@ export default class StatusClient {
 		return axios.get(this.baseUrl + "/api/services");
 	}
 
+	createService(service) {
+		return axios.post(this.baseUrl + "/api/services", service);
+	}
+
+	getService(serviceId) {
+		return axios.get(this.baseUrl + "/api/services/" + serviceId);
+	}
+
 	getIncidents() {
 		return axios.get(this.baseUrl + "/api/incidents");
 	}
 
+	getActiveIncidents() {
+		return axios.get(this.baseUrl + "/api/incidents/active");
+	}
+
 	getIncident(incidentId) {
 		return axios.get(this.baseUrl + "/api/incidents/" + incidentId);
+	}
+
+	createIncident(incident) {
+		return axios.post(this.baseUrl + "/api/incidents", incident);
+	}
+
+	updateIncident(incidentId, update) {
+		return axios.post(this.baseUrl + "/api/incidents/" + incidentId, update);
 	}
 }
