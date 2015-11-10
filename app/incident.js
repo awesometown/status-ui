78d98df1c6c68d9cca816b765952b2cd6f60b3fb
John Price <joprice@ea.com>
2015-11-10 17:08:08 -0500

    Resolve service name on details page

--- app/incident.js
+++ app/incident.js
@@ -6,21 +6,13 @@ import moment from "moment";
 
 export default React.createClass({
 	getInitialState: function () {
-		return {
-			incident: {
-				title: "",
-				affectedServiceIds: [],
-				incidentUpdates: [],
-				createdAt: "",
-				updatedAt: ""
-			}
-		};
+		return {loaded: false}
 	},
 
 	componentDidMount: function () {
-		var location = window.location.href;
-		var instanceId = location.substring(location.lastIndexOf('/') + 1);
-		StatusClient.getIncident(instanceId)
+		let location = window.location.href;
+		let instanceId = location.substring(location.lastIndexOf('/') + 1);
+		let incidentPromise = StatusClient.getIncident(instanceId)
 			.then(result => {
 				if (this.isMounted()) {
 					this.setState({incident: result.data});
@@ -28,46 +20,62 @@ export default React.createClass({
 			}).catch(result => {
 				console.log(result);
 			});
+
+		let servicesPromise = StatusClient.getServices()
+			.then(result => {
+				let servicesMap = {};
+				for (let service of result.data.data) {
+					servicesMap[service.id] = service.name;
+				}
+				this.setState({servicesMap: servicesMap});
+			}).catch(result => console.log(result));
+		Promise.all([incidentPromise, servicesPromise])
+			.then(values => this.setState({loaded: true}));
 	},
 
 	render: function () {
 		var incident = this.state.incident;
-		return (
-			<div id="incident-details">
+		let content = <div id="incident-details"/>
+		if (this.state.loaded) {
+			content = (
+				<div id="incident-details">
 
-				<Grid>
-					<Row>
-						<Col md={12}>
-							<Link to="/">Back</Link>
-						</Col>
-					</Row>
-					<Row>
-						<Col md={8}>
-							<h1>{incident.title}</h1>
-						</Col>
-						<Col md={4}>
-							<p>Affected Services:</p>
-							<ServicesList serviceIds={incident.affectedServiceIds}/>
-						</Col>
-					</Row>
-					<Row>
-						<LatestDescription md={12} incident={incident}/>
-					</Row>
-					<Row>
-						<Col md={12}>
-							<h3>Updates:</h3>
-							<UpdateList updates={incident.incidentUpdates}/>
-						</Col>
-					</Row>
-				</Grid>
-			</div>
-		);
+					<Grid>
+						<Row>
+							<Col md={12}>
+								<Link to="/">Back</Link>
+							</Col>
+						</Row>
+						<Row>
+							<Col md={8}>
+								<h1>{incident.title}</h1>
+							</Col>
+							<Col md={4}>
+								<p>Affected Services:</p>
+								<ServicesList servicesMap={this.state.servicesMap} serviceIds={incident.affectedServiceIds}/>
+							</Col>
+						</Row>
+						<Row>
+							<LatestDescription md={12} incident={incident}/>
+						</Row>
+						<Row>
+							<Col md={12}>
+								<h3>Updates:</h3>
+								<UpdateList updates={incident.incidentUpdates}/>
+							</Col>
+						</Row>
+					</Grid>
+				</div>
+			);
+		}
+		return content;
 	}
 });
 
 var ServicesList = React.createClass({
 	render: function () {
-		var serviceNodes = this.props.serviceIds.map(serviceId => <Service key={serviceId} serviceId={serviceId}/>);
+		let servicesMap = this.props.servicesMap;
+		var serviceNodes = this.props.serviceIds.map(serviceId => <Service key={serviceId} serviceName={servicesMap[serviceId]} serviceId={serviceId}/>);
 		return (
 			<ul>
 				{serviceNodes}
@@ -79,7 +87,7 @@ var ServicesList = React.createClass({
 var Service = React.createClass({
 	render: function () {
 		return (
-			<li key={this.props.serviceId}>{this.props.serviceId}</li>
+			<li key={this.props.serviceId}>{this.props.serviceName}</li>
 		);
 	}
 });
@@ -92,7 +100,8 @@ var LatestDescription = React.createClass({
 			<Col md={this.props.md}>
 				<p>{this.props.incident.state} - {latestUpdate}</p>
 
-				<p>Opened {moment(this.props.incident.createdAt).fromNow()}; last updated {moment(this.props.incident.updatedAt).fromNow()}</p>
+				<p>Opened {moment(this.props.incident.createdAt).fromNow()}; last
+					updated {moment(this.props.incident.updatedAt).fromNow()}</p>
 			</Col>);
 	}
 });
